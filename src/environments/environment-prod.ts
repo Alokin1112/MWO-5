11f0f95395b31f03bc91970b191b61832a822b6e
Dawid Szczepankowski <dawid.szczepankowski@elzab.pl>
2023-11-08 22:30:31 +0100

    added configuration

---
+++ src/environments/environment-prod.ts
@@ -0,0 +1,9 @@
+import { Environment } from '@env/environment.interface';
+
+export const environment: Environment = {
+  production: false,
+  httpBackend: "http://dataservice.accuweather.com",
+  httpBackendPersonal: 'http://testApipersonal.com',
+  language: 'pl',
+  apiKey: 'eDhUv4u2hw0YxdGuvV1CIal7aIdsskcn'
+};
